3c7c1fa9babd6c058440a758a3153af1b7b74ba6
Clara <clari.allende@gmail.com>
2013-11-09 12:26:15 -0300

    Abm de afiliados conteo de intentos de login

--- Clinica Frba/Registro de Usuario/Usuario.cs
+++ Clinica Frba/Registro de Usuario/Usuario.cs
@@ -8,9 +8,17 @@ namespace Clinica_Frba.Registro_de_Usuario
 {
     class Usuario
     {
-        public string username;
-        protected string password;
+        public String username;
+        protected String password;
         public Rol rol;
-        
+        private int loginAttempsCount;   
+
+    public void login(String name, String password){
+        //llamar al script
+         if (!true) loginAttempsCount++;
+        if (loginAttempsCount == 3) {
+            //block?
+            //reset counter
+            loginAttempsCount = 0;
     }
 }
