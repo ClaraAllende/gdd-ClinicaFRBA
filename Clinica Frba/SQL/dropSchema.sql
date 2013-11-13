a133e8d7eb544108010d599f7a3f2c94932e9078
Clara <clari.allende@gmail.com>
2013-11-13 00:28:01 -0300

    agregados scripts de create/drop schema

---
+++ Clinica Frba/SQL/dropSchema.sql
@@ -0,0 +1,6 @@
+USE [GD2C2013]
+GO
+--ejecutar el script despuès de dropear las tablas, si no hay que modificar esta consulta
+--para hacer el drop de las tablas, antes de dropear el schema.
+DROP SCHEMA HAKUNA_MATATA
+GO
