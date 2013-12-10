1dac06d10548b5099e7fd203152c84ea49199fb3
NicolasRaus <nicolasraus@gmail.com>
2013-12-10 10:40:21 -0300

    ABM PEDIDO TURNO funcion para listar profesionales por una especialidad recibida por parametro

--- Clinica Frba/SQL/Pedido_de_turno.sql
+++ Clinica Frba/SQL/Pedido_de_turno.sql
@@ -28,5 +28,29 @@ BEGIN
 END
 GO
 
---DROP FUNCTION HAKUNA_MATATA.FN_get_profesionales
---DROP FUNCTION HAKUNA_MATATA.FN_get_especialidades
+CREATE FUNCTION HAKUNA_MATATA.FN_get_profesionales_por_especialidad
+	(@descripcion_especialidad VARCHAR(255)
+	)
+RETURNS @tabla TABLE
+	(nombre_y_apellido VARCHAR(510)
+	)
+AS
+BEGIN
+	INSERT INTO @tabla
+	SELECT P.nombre + P.apellido
+	FROM HAKUNA_MATATA.Especialidad E
+	JOIN HAKUNA_MATATA.EspecialidadXProfesional EP ON (	E.descripcion = @descripcion_especialidad	AND
+														E.id_especialidad = EP.id_especialidad		AND
+														E.habilitada = 1							AND
+														EP.habilitada = 1)
+	JOIN HAKUNA_MATATA.Profesional P ON (EP.matricula = P.matricula	AND
+										 P.habilitada = 1)
+	RETURN
+END
+GO
+
+/*
+DROP FUNCTION HAKUNA_MATATA.FN_get_profesionales
+DROP FUNCTION HAKUNA_MATATA.FN_get_especialidades
+DROP FUNCTION HAKUNA_MATATA.FN_get_profesionales_por_especialidad
+*/
